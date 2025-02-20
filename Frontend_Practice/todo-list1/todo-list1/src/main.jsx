9fae14f6b20846d9fd6318cbf4ce19ea3dcab9f6
toketime <flatoke@gmail.com>
2025-02-20 19:24:29 +0900

    Add: contextAPI done

--- Frontend_Practice/todo-list1/todo-list1/src/main.jsx
+++ Frontend_Practice/todo-list1/todo-list1/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Todo } from './pages/Todo.jsx'
+import { TodoProvider } from './contexts/TodoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Todo />
+    {/* TodoProviderでTodoContextをラップ */}
+    {/* TodoProviderの中にTodoコンポーネントを配置 */}
+    <TodoProvider>
+      <Todo />
+    </TodoProvider>
   </StrictMode>,
 )
